90f56332b97944bab14b7d8f3bad17446a9d0495
Cole Rottenberg <cole.rottenberg@gmail.com>
2023-11-19 02:56:41 -0500

    fin

--- projection.m
+++ projection.m
@@ -1,14 +1,14 @@
 function [y_hat,z] = projection(y,u)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Name: (type your name here)      % 
-% UF ID: ####-####                 %
+% Name: Cole Rottenberg            % 
+% UF ID: 1106-2528                 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This function determines (write the purpose of the function here) 
 % Input arguments: (indicate the input arguments here)
 % Output argument: (indicate the output arguments here)
 %
 
-y_hat = ;
-z = ;
+y_hat = (dot(y,u)/dot(u,u))*u ;
+z = y - y_hat;
 end
